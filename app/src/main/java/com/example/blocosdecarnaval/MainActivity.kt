248f2fa04a7c7673505bca34d08d27c9b3dea96d
FernandoNakasone <fernandonakasone040@gmail.com>
2026-02-19 09:23:37 -0300

    App Blocos de Carnaval

---
+++ app/src/main/java/com/example/blocosdecarnaval/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.blocosdecarnaval
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blocosdecarnaval.ui.theme.BlocosDeCarnavalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BlocosDeCarnavalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image()
+        Column() {  }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BlocosDeCarnavalTheme {
+        Greeting("Android")
+    }
+}
